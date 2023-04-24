4d868aee319c85321dc3d4f4a682b86f0693ef05
Jordi Jolink <mail@jordijolink.nl>
2023-04-24 21:59:33 +0200

    Fix production endpoints

--- DpdShipper/DpdShipper.Api/Endpoints/ProductionEndpoints.cs
+++ DpdShipper/DpdShipper.Api/Endpoints/ProductionEndpoints.cs
@@ -2,6 +2,7 @@
 
 internal class ProductionEndpoints : IEndpoints
 {
-    public string LoginService => "https://wsshipper.dpd.nl/PublicApi/services/LoginService/V2_1";
-    public string ShipmentService => "https://wsshipper.dpd.nl/PublicApi/services/ShipmentService/V3_3";
+    public string LoginService => "https://wsshipper.dpd.nl/soap/services/LoginService/V2_1";
+
+    public string ShipmentService => "https://wsshipper.dpd.nl/soap/services/ShipmentService/V3_3/";
 }
